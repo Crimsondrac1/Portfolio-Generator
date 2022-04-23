3dfd2c4369e6fff09a772eb950f3779952d83d00
crimsondrac1 <doug@chillopolis.com>
2022-04-23 17:12:49 -0500

    Implemented and tested Inquirer

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+// const pageHTML = generatePage(uName, github);
+
+//   fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+  
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
+
+// const profileDataArgs = process.argv.slice(2);
 // const uName = profileDataArgs[0];
 // const github = profileDataArgs[1];
-const [uName, github] = profileDataArgs;
+// const [uName, github] = profileDataArgs;
 // console.log(profileDataArgs);
 // const generatePage = () => 'Name: Doug, Github: hub';
 
-const pageHTML = generatePage(uName, github);
-
-  fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw err;
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
-
-// const printProfileData = profileDataArr => {
+  // const printProfileData = profileDataArr => {
 //     // This...
 //     for (let i = 0; i < profileDataArr.length; i += 1) {
 //       console.log(profileDataArr[i]);
